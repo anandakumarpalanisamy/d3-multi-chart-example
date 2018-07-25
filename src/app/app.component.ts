6f6b24e7c14c782dc528f21806094a8c220db3cf
Palanisamy Anandakumar (JBF5BRJ) <JBF5BRJ@tus.ams1907.com>
2018-07-25 06:37:56 -0400

    Add grouped bar plus multi line - d3 graph examples.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,214 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import { Data } from './data/data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  margin;
+  width;
+  height;
+  data = Data;
+  extentData = [];
+  hoursData = [];
+  hoursKey = ['Total Office Hrs', 'Total Productive Hrs'];
+  punchKeys = ['In Time', 'Out Time'];
+
+  constructor() {
+    this.margin = { top: 20, right: 50, bottom: 80, left: 60 };
+    this.width = 1100 - this.margin.left - this.margin.right;
+    this.height = 550 - this.margin.top - this.margin.bottom;
+  }
+
+  ngOnInit() {
+    const self = this;
+    const extentData = this.extentData;
+    const hoursData = this.hoursData;
+    this.data.forEach(function(d) {
+      d.actualDate = d3.time.format('%Y-%m-%d').parse(d.actualDate);
+      d.in = d3.time.format('%Y-%m-%dT%H:%M:%S').parse(d.in);
+      d.out = d3.time.format('%Y-%m-%dT%H:%M:%S').parse(d.out);      
+      extentData.push(d.in);
+      extentData.push(d.out);
+      hoursData.push(d.totalHours);
+      hoursData.push(d.totalProductiveHours);
+    });
+  }
+
+  ngAfterViewInit() {
+
+    const self = this;
+    const height = self.height;
+    const width = self.width;
+    const margin = self.margin;
+    
+    const x = d3.time.scale().range([0, this.width]);
+    const x0 = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+
+    const y1 = d3.time.scale().range([this.height, 0]);
+
+    const y2 = d3.scale.linear().range([this.height, 0]);
+
+    const z = d3.scale.ordinal()
+      .range(['#2d45fc', '#43ad48']);
+
+    const w = d3.scale.ordinal()
+      .range(['#e56429', '#e59628']);
+
+    const xAxis = d3.svg.axis().scale(x).orient('bottom')
+      .ticks(30)
+      .tickFormat(d3.time.format('%b-%d'));
+
+    const y1Axis = d3.svg.axis().scale(y1).orient('left')
+      .ticks(d3.time.hour, 1)
+      .tickFormat(d3.time.format('%H %M'));
+
+    const y2Axis = d3.svg.axis().scale(y2).orient('right');
+
+    const valueline1 = d3.svg.line()
+      .x(function (d) { return x(d.actualDate); })
+      .y(function (d) { return y1(d.in); });
+
+    const valueline2 = d3.svg.line()
+      .x(function (d) { return x(d.actualDate); })
+      .y(function (d) { return y1(d.out); });
+  
+    const svg = d3.select('#chart')
+      .append('svg')
+        .attr('width', this.width + this.margin.left + this.margin.right)
+        .attr('height', this.height + this.margin.top + this.margin.bottom)
+      .append('g')
+        .attr('transform', 
+          'translate(' + this.margin.left + ',' + this.margin.top + ')');
+
+    x.domain(d3.extent(this.data, function(d) { return d.actualDate; }));
+    x0.domain(this.hoursKey.map(function (d) { return d; }));
+
+    y1.domain(d3.extent(this.extentData, function(d) { return d; }));
+    y2.domain([0, d3.max(this.hoursData, function(d) { return d; })]);
+
+    svg.selectAll('.productive')
+      .data(this.data)
+    .enter().append('rect')
+      .style('fill', '#2d45fc')
+      .attr('x', function(d) { return x(d.actualDate); })
+      .attr('width', 10)
+      .attr('y', function(d) { return y2(d.totalProductiveHours); })
+      .attr('height', function(d) { return height - y2(d.totalProductiveHours); });
+
+    svg.selectAll('.total')
+      .data(this.data)
+    .enter().append('rect')
+      .style('fill', '#43ad48')
+      .attr('x', function(d) { return x(d.actualDate); })
+      .attr('width', 5)
+      .attr('y', function(d) { return y2(d.totalHours); })
+      .attr('height', function(d) { return height - y2(d.totalHours); });
+
+    svg.append('path')
+      .style('stroke', '#e56429')
+      .attr('d', valueline1(this.data));
+
+    svg.append('path')
+      .style('stroke', '#e59628')
+      .attr('d', valueline2(this.data));
+
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis).selectAll('text').attr('y', 0)
+      .attr('x', '-40')
+      .attr('dy', '.25em')
+      .attr('transform', 'rotate(-50)')
+      .style('text-anchor', 'start');
+
+    svg.append('text')
+      .attr('transform',
+        'translate(' + (width/2) + ' ,' + (height + margin.top + 20) + ')')
+      .style('text-anchor', 'middle')
+      .style('font-weight', 600)
+      .text('Days');
+
+    svg.append('g')
+      .attr('class', 'y axis')
+      .call(y1Axis);
+
+    svg.append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 0 - margin.left)
+      .attr('x',0 - (height / 2))
+      .attr('dy', '1em')
+      .style('text-anchor', 'middle')
+      .style('font-weight', 600)
+      .text('Time (HH:MM)');
+
+    const group = svg.selectAll('.group')
+      .data(this.hoursKey)
+      .enter().append('g')
+        .attr('class', 'group')
+        .attr('transform', function (d) { return 'translate(' + x0(d) + ',0)'; });
+
+
+    svg.append('g')
+      .attr('class', 'y axis')
+      .attr('transform', 'translate(' + (width + 11) + ' ,0)')
+      .call(y2Axis)
+      .attr('dy', '-.1em')
+
+    svg.append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y',  (width + 35))
+      .attr('x', 0 - (height / 2))
+      .attr('dy', '1em')
+      .style('text-anchor', 'middle')
+      .style('font-weight', 600)
+      .text('No. of Hours');
+
+    const barLegend = svg.append('g')
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', 10)
+      .attr('text-anchor', 'end')
+        .selectAll('g')
+        .data(this.hoursKey.slice().reverse())
+        .enter().append('g')
+          .attr('transform', function(d, i) { return 'translate(0,' + i * 20 + ')'; });
+
+    barLegend.append('rect')
+      .attr('x', width - 19)
+      .attr('width', 19)
+      .attr('height', 19)
+      .attr('fill', z);
+    
+    barLegend.append('text')
+      .attr('x', width - 24)
+      .attr('y', 9.5)
+      .attr('dy', '0.32em')
+      .text(function(d) { return d; });
+
+    const lineLegend = svg.append('g')
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', 10)
+      .attr('text-anchor', 'end')
+        .selectAll('g')
+        .data(this.punchKeys.slice().reverse())
+        .enter().append('g')
+          .attr('transform', function(d, i) { return 'translate(0,' + (i + 3) * 15 + ')'; });
+
+    lineLegend.append('rect')
+      .attr('x', width - 19)
+      .attr('width', 19)
+      .attr('height', 5)
+      .attr('fill', w);
+    
+    lineLegend.append('text')
+      .attr('x', width - 19)
+      .attr('y', 3.5)
+      .attr('dy', '0.32em')
+      .text(function(d) { return d; });
+  }
+
 }
---
+++ src/app/data/data.ts
@@ -0,0 +1,212 @@
+export const Data = [
+    {
+        in: '2018-07-01T11:00:00',
+        out: '2018-07-01T20:00:00',
+        totalHours: 10,
+        totalProductiveHours: 7.5,
+        actualDate: '2018-07-01'
+    },
+    {
+        in: '2018-07-01T13:00:00',
+        out: '2018-07-01T21:00:00',
+        totalHours: 9,
+        totalProductiveHours: 9,
+        actualDate: '2018-07-02'
+    },
+    {
+        in: '2018-07-01T12:15:00',
+        out: '2018-07-01T20:30:00',
+        totalHours: 8,
+        totalProductiveHours: 5,
+        actualDate: '2018-07-03'
+    },
+    {
+        in: '2018-07-01T11:15:00',
+        out: '2018-07-01T20:30:00',
+        totalHours: 11.5,
+        totalProductiveHours: 7.5,
+        actualDate: '2018-07-04'
+    },
+    {
+        in: '2018-07-01T12:15:00',
+        out: '2018-07-01T20:30:00',
+        totalHours: 14,
+        totalProductiveHours: 8,
+        actualDate: '2018-07-05'
+    },
+    {
+        in: '2018-07-01T10:15:00',
+        out: '2018-07-01T19:30:00',
+        totalHours: 11,
+        totalProductiveHours: 10.5,
+        actualDate: '2018-07-06'
+    },
+    {
+        in: '2018-07-01T13:15:00',
+        out: '2018-07-01T20:20:00',
+        totalHours: 10.5,
+        totalProductiveHours: 9.5,
+        actualDate: '2018-07-07'
+    },
+    {
+        in: '2018-07-01T10:10:00',
+        out: '2018-07-01T19:45:00',
+        totalHours: 11,
+        totalProductiveHours: 8,
+        actualDate: '2018-07-08'
+    },
+    {
+        in: '2018-07-01T11:10:00',
+        out: '2018-07-01T19:30:00',
+        totalHours: 12,
+        totalProductiveHours: 7,
+        actualDate: '2018-07-09'
+    },
+    {
+        in: '2018-07-01T09:15:00',
+        out: '2018-07-01T18:20:00',
+        totalHours: 9.5,
+        totalProductiveHours: 7.5,
+        actualDate: '2018-07-10'
+    },
+    {
+        in: '2018-07-01T09:45:00',
+        out: '2018-07-01T19:30:00',
+        totalHours: 7.5,
+        totalProductiveHours: 5.5,
+        actualDate: '2018-07-11'
+    },
+    {
+        in: '2018-07-01T11:15:00',
+        out: '2018-07-01T20:10:00',
+        totalHours: 9,
+        totalProductiveHours: 2,
+        actualDate: '2018-07-12'
+    },
+    {
+        in: '2018-07-01T11:35:00',
+        out: '2018-07-01T20:05:00',
+        totalHours: 15,
+        totalProductiveHours: 12,
+        actualDate: '2018-07-13'
+    },
+    {
+        in: '2018-07-01T12:30:00',
+        out: '2018-07-01T20:40:00',
+        totalHours: 18,
+        totalProductiveHours: 14,
+        actualDate: '2018-07-14'
+    },
+    {
+        in: '2018-07-01T12:45:00',
+        out: '2018-07-01T22:30:00',
+        totalHours: 14.6,
+        totalProductiveHours: 12.3,
+        actualDate: '2018-07-15'
+    },
+    {
+        in: '2018-07-01T11:10:00',
+        out: '2018-07-01T21:00:00',
+        totalHours: 16.5,
+        totalProductiveHours: 12.2,
+        actualDate: '2018-07-16'
+    },
+    {
+        in: '2018-07-01T12:00:00',
+        out: '2018-07-01T16:00:00',
+        totalHours: 9.5,
+        totalProductiveHours: 8.5,
+        actualDate: '2018-07-17'
+    },
+    {
+        in: '2018-07-01T12:15:00',
+        out: '2018-07-01T21:30:00',
+        totalHours: 1.5,
+        totalProductiveHours: 1,
+        actualDate: '2018-07-18'
+    },
+    {
+        in: '2018-07-01T11:15:00',
+        out: '2018-07-01T20:05:00',
+        totalHours: 5,
+        totalProductiveHours: 3.8,
+        actualDate: '2018-07-19'
+    },
+    {
+        in: '2018-07-01T13:15:00',
+        out: '2018-07-01T17:30:00',
+        totalHours: 14.6,
+        totalProductiveHours: 13.6,
+        actualDate: '2018-07-20'
+    },
+    {
+        in: '2018-07-01T15:15:00',
+        out: '2018-07-01T19:30:00',
+        totalHours: 18,
+        totalProductiveHours: 17,
+        actualDate: '2018-07-21'
+    },
+    {
+        in: '2018-07-01T10:15:00',
+        out: '2018-07-01T18:30:00',
+        totalHours: 16,
+        totalProductiveHours: 15.5,
+        actualDate: '2018-07-22'
+    },
+    {
+        in: '2018-07-01T10:10:00',
+        out: '2018-07-01T19:30:00',
+        totalHours: 9.5,
+        totalProductiveHours: 8.7,
+        actualDate: '2018-07-23'
+    },
+    {
+        in: '2018-07-01T11:40:00',
+        out: '2018-07-01T20:30:00',
+        totalHours: 8.7,
+        totalProductiveHours: 8.3,
+        actualDate: '2018-07-24'
+    },
+    {
+        in: '2018-07-01T08:15:00',
+        out: '2018-07-01T21:30:00',
+        totalHours: 9.5,
+        totalProductiveHours: 8.5,
+        actualDate: '2018-07-25'
+    },
+    {
+        in: '2018-07-01T10:35:00',
+        out: '2018-07-01T22:30:00',
+        totalHours: 11,
+        totalProductiveHours: 10.3,
+        actualDate: '2018-07-26'
+    },
+    {
+        in: '2018-07-01T11:15:00',
+        out: '2018-07-01T19:30:00',
+        totalHours: 12.2,
+        totalProductiveHours: 8.9,
+        actualDate: '2018-07-27'
+    }/* ,
+    {
+        in: '2018-07-01T11:25:00',
+        out: '2018-07-01T19:50:00',
+        totalHours: 8,
+        totalProductiveHours: 5.5,
+        actualDate: '2018-07-28'
+    },
+    {
+        in: '2018-07-01T12:30:00',
+        out: '2018-07-01T20:50:00',
+        totalHours: 7.6,
+        totalProductiveHours: 7,
+        actualDate: '2018-07-29'
+    },
+    {
+        in: '2018-07-01T12:45:00',
+        out: '2018-07-01T17:45:00',
+        totalHours: 15,
+        totalProductiveHours: 13.5,
+        actualDate: '2018-07-30'
+    } */
+];
